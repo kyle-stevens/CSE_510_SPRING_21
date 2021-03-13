2c71aae7d066854282e0c601a6a70e1234e4d426
kyle-stevens <43458322+kyle-stevens@users.noreply.github.com>
2021-03-13 13:58:48 -0700

    Exception Catching

--- minjava/javaminibase/src/iterator/BTreeSky.java
+++ minjava/javaminibase/src/iterator/BTreeSky.java
@@ -1,43 +1,45 @@
 package iterator;
 
-//Until better solution is found
-import java.util.ArrayList;
+import java.util.Vector;
 import java.util.List;
-
 import java.io.IOException;
 
 import bufmgr.PageNotReadException;
+
 import global.AttrType;
 import global.IndexType;
 import global.PageId;
+import global.GlobalConst;
+import global.RID;
 import heap.Heapfile;
 import heap.InvalidTupleSizeException;
 import heap.InvalidTypeException;
 import heap.Tuple;
+import heap.Scan;
+import heap.InvalidSlotNumberException;
+import heap.FileAlreadyDeletedException;
+import heap.HFBufMgrException;
 import index.IndexException;
 import index.IndexScan;
 import index.UnknownIndexTypeException;
+
 
 public class BTreeSky extends Iterator{
 
 
 
         //Operations Buffer
-        private byte[] buffer;
+        private byte[][] buffer;
         //SkyLine buffer
         private OBuf oBuf;
         //Sprojection Object declaration
         private FldSpec[] Sprojection;
         //dominating Tuple
         private Tuple oneTupleToRuleThemAll;
-        //tuples encountered temp list
-        private ArrayList<Tuple> tuplesEncountered;
         //Iterator for tuples
         private Iterator[] iter;
         //Heapfile for disk storage
         private Heapfile heap;
-        //Obuf creation for storage in main memory
-        private OBufSortSky oBuf;
         //BlockNestedLoopSky instance
         private BlockNestedLoopSky bNLS;
         //Final Skyline
@@ -52,11 +54,13 @@ public class BTreeSky extends Iterator{
         private short _len_in1;
         private int _n_out_flds;
         private FldSpec[] _proj_list;
-        private CondExpr _outFilter;
+        private CondExpr[] _outFilter;
+        private Iterator scan;
+        private int MINIBASE_PAGESIZE = 1024;
 
         //Pass in Sorted Index Files Descending Order
         public BTreeSky(AttrType[] in1,
-                        int len_in1,
+                        short len_in1,
                         short[] t1_str_sizes,
                         Iterator am1,
                         java.lang.String relationName,
@@ -70,7 +74,9 @@ public class BTreeSky extends Iterator{
                         UnknownIndexTypeException,
                         IOException,
                         SortException,
-                        IteratorBMException
+                        IteratorBMException, JoinsException, IndexException, InvalidTupleSizeException,
+                			 PageNotReadException, TupleUtilsException, PredEvalException, SortException,
+                			LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception
         {
             file_name = "skyline_candidates";
             //Initialize Heap file to one named 'skyline_candidates'
@@ -84,7 +90,7 @@ public class BTreeSky extends Iterator{
             //Initialize FldSpec object for use in iteration
             Sprojection = new FldSpec[len_in1];
             //Initialize CondExpr array;
-            cExpr = new CondExpr[len_in];
+            cExpr = new CondExpr[len_in1];
             //Create iterator array
             iter = new Iterator[len_in1];
             //Iterate over IndexFiles and create separate iterators and fldspecs
@@ -92,7 +98,8 @@ public class BTreeSky extends Iterator{
                     Sprojection[i] = new FldSpec(new RelSpec(RelSpec.outer), i+1);
                     cExpr[i] = new CondExpr();
                     iter[i] = new IndexScan(new IndexType(IndexType.B_Index),
-                    relationName, index_file_list[i], in1[i], t1_str_sizes,len_in1,len_in1,Sprojection, null, 0, false);
+                    relationName, index_file_list[i], in1, t1_str_sizes,len_in1,
+                    len_in1,Sprojection, null, 0, false);
             }
             _in1 = in1;
             _s1_sizes = t1_str_sizes;
@@ -101,9 +108,11 @@ public class BTreeSky extends Iterator{
             _proj_list = Sprojection;
             _outFilter = cExpr;
             //Get Iterator for Heapfile
-            fScan = new FileScan(_file_name, _in1, _s1_sizes, _len_in1, _n_out_flds, _proj_list, _outFilter);
+            fScan = new FileScan(file_name, _in1, _s1_sizes, _len_in1,
+            _n_out_flds, _proj_list, _outFilter);
             //Initialize BNLS object to use heapfile
-            bNLS = new BlockNestedLoopSky(in1, len_in1, t1_str_sizes, fScan, "skyline_candidates", pref_list, pref_list_length,
+            bNLS = new BlockNestedLoopSky(in1, len_in1, t1_str_sizes, fScan,
+            "skyline_candidates", pref_list, pref_list_length,
             n_pages);
         }
 
@@ -124,15 +133,17 @@ public class BTreeSky extends Iterator{
                 boolean common = false;
                 boolean foundDominantTuple = false;
                 boolean duplicate = false;
+                scan = iter[0]; //setting up get_next
                 //Loop over the tuples of the first index_file ( Tuple Field )
                 while((temp=iter[0].get_next()) != null && !foundDominantTuple){
                     //Reset Loop Conition
                     common = false;
-
+                    scan = iter[0]; //resetting get_next
                     //Iterate over all iterators for each index file
                     for(int i = 1; i < iter.length; i++){
                         //Reset Loop condition
                         common = false;
+                        scan = iter[i]; //setting get_next
                         //So Long as no common value has been Found
                         //and there exists some next tuple in the list
                         while(((temp2=iter[i].get_next())!= null) && !common){
@@ -160,7 +171,7 @@ public class BTreeSky extends Iterator{
                                     Tuple heap_dup;
                                     RID heap_dup_rid = new RID();
                                     //iterate over heap file.
-                                    while(heap_dup = scd.getNext(heap_dup_rid) != null){
+                                    while((heap_dup = scd.getNext(heap_dup_rid))!= null){
                                         if(heap_dup == temp2){
                                             duplicate = true;
                                             break;
@@ -181,7 +192,7 @@ public class BTreeSky extends Iterator{
                         foundDominantTuple = true;
                     }
                 }
-            }
+
             //flush the buffer and store to heapfile
             oBuf.flush();
             //retrieve the calculated skyline
@@ -191,14 +202,32 @@ public class BTreeSky extends Iterator{
             //return calculated skyline
             return skyline;
         }
+        @Override
+        public Tuple get_next(){
+                Tuple t;
+                try{
+                        if((t=scan.get_next()) != null){
+                                return(t);
+                        }
+                }
+                catch(Exception e){
+                        return(null);
+                }
+                return(null);
 
+
+        }
         @Override
         public void close() throws IOException, JoinsException, SortException, IndexException {
 	// TODO Auto-generated method stub
-	       iter.close();
-	       try {
-	           new Heapfile(oBuf.getCurr_file() + (number_of_run - 1)).deleteFile();
-	       } catch (Exception e) {
-               }
+                try{
+                        heap.deleteFile();
+                }
+                catch (Exception e){
+                        return;
+                }
+
+
+
         }
 }
