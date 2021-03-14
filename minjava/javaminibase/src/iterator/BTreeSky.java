a5dcce37013f881cd7501a55c85d2f026ef51485
Kyle <43458322+kyle-stevens@users.noreply.github.com>
2021-03-14 14:13:27 -0700

    Update BTreeSky.java

--- minjava/javaminibase/src/iterator/BTreeSky.java
+++ minjava/javaminibase/src/iterator/BTreeSky.java
@@ -49,14 +49,20 @@ public class BTreeSky extends Iterator{
         //CondExpr for FileScan
         private CondExpr[] cExpr;
         private String file_name;
-        private AttrType _in1[];
-        private short _s1_sizes[];
-        private short _len_in1;
+
+
+
         private int _n_out_flds;
         private FldSpec[] _proj_list;
         private CondExpr[] _outFilter;
         private Iterator scan;
         private int MINIBASE_PAGESIZE = 1024;
+        private PageId[] bufferPIDs;
+        private int _n_Pages;
+        private AttrType[] _in_1;
+        private short _len_in1;
+        private short[] _t1_str_sizes;
+
 
         //Pass in Sorted Index Files Descending Order
         public BTreeSky(AttrType[] in1,
@@ -78,45 +84,62 @@ public class BTreeSky extends Iterator{
                 			 PageNotReadException, TupleUtilsException, PredEvalException, SortException,
                 			LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception
         {
+            this._len_in1 = len_in1;
+            this._in_1 = in1;
+            this._t1_str_sizes = t1_str_sizes;
+            this._n_Pages = n_pages;
             file_name = "skyline_candidates";
             //Initialize Heap file to one named 'skyline_candidates'
             heap = new Heapfile(file_name);
-            //Create new OBuf object for a computation buffer
-            oBuf = new OBuf();
             //Initialize Buffer
             buffer = new byte[n_pages][];
+            bufferPIDs = new PageId[n_pages];
+            get_buffer_pages(n_pages, bufferPIDs,buffer);
+            //Create new OBuf object for a computation buffer
+            oBuf = new OBuf();
+            Tuple w = new Tuple();
+            w.setHdr(this._len_in1, this._in_1, this._t1_str_sizes);
             //Initialize OBuf object to include heapfile storage on flushed
-            oBuf.init(buffer, n_pages, MINIBASE_PAGESIZE, heap, true);
+            oBuf.init(buffer, n_pages, w.size(), heap, false);
             //Initialize FldSpec object for use in iteration
             Sprojection = new FldSpec[len_in1];
             //Initialize CondExpr array;
-            cExpr = new CondExpr[len_in1];
+            cExpr = new CondExpr[pref_list_length];
             //Create iterator array
-            iter = new Iterator[len_in1];
+            iter = new Iterator[pref_list_length];
             //Iterate over IndexFiles and create separate iterators and fldspecs
             for(int j = 0; j<len_in1;j++){
 		    Sprojection[j] = new FldSpec(new RelSpec(RelSpec.outer), j+1);
 	    }
-	    for(int i=0; i<len_in1;i++){
+	    for(int i=0; i<pref_list_length;i++){
                     cExpr[i] = new CondExpr();
                     iter[i] = new IndexScan(new IndexType(IndexType.B_Index),
-                    relationName, index_file_list[i], in1, t1_str_sizes,len_in1,
-                    len_in1,Sprojection, null, 0, false);
+                        relationName,
+                        index_file_list[i],
+                        in1,
+                        t1_str_sizes,
+                        len_in1,
+                        len_in1,
+                        Sprojection,
+                        null,
+                        0,
+                        false);
             }
-            _in1 = in1;
-            _s1_sizes = t1_str_sizes;
+            this._in_1 = in1;
+
             _len_in1 = len_in1;
             _n_out_flds = pref_list_length;
             _proj_list = Sprojection;
             _outFilter = cExpr;
             //Get Iterator for Heapfile
-            fScan = new FileScan(file_name, _in1, _s1_sizes, _len_in1,
+            fScan = new FileScan(file_name, this._in_1, _t1_str_sizes, _len_in1,
             _n_out_flds, _proj_list, _outFilter);
             //Initialize BNLS object to use heapfile
+            runSky();
             bNLS = new BlockNestedLoopSky(in1, len_in1, t1_str_sizes, fScan,
             "skyline_candidates", pref_list, pref_list_length,
             n_pages);
-            runSky();
+
         }
 
         /**
@@ -136,20 +159,23 @@ public class BTreeSky extends Iterator{
                 boolean common = false;
                 boolean foundDominantTuple = false;
                 boolean duplicate = false;
-                scan = iter[0]; //setting up get_next
+                //scan = iter[0]; //setting up get_next
                 //Loop over the tuples of the first index_file ( Tuple Field )
                 while((temp=iter[0].get_next()) != null && !foundDominantTuple){
+                    temp.setHdr(this._len_in1, this._in_1, this._t1_str_sizes);
+
                     //Reset Loop Conition
                     common = false;
-                    scan = iter[0]; //resetting get_next
+                    //scan = iter[0]; //resetting get_next
                     //Iterate over all iterators for each index file
                     for(int i = 1; i < iter.length; i++){
                         //Reset Loop condition
                         common = false;
-                        scan = iter[i]; //setting get_next
+                        //scan = iter[i]; //setting get_next
                         //So Long as no common value has been Found
                         //and there exists some next tuple in the list
                         while(((temp2=iter[i].get_next())!= null) && !common){
+                                temp2.setHdr(this._len_in1, this._in_1, this._t1_str_sizes);
                             //If we have found a common tuple, end this loop iteration
                             //via the loop condition
                             if (temp == temp2){
@@ -163,6 +189,7 @@ public class BTreeSky extends Iterator{
                             else if(temp != temp2){
                                 common = false;
                                 while((dup = oBuf.Get()) != null){
+                                    dup.setHdr(this._len_in1, this._in_1, this._t1_str_sizes);
                                     if(dup == temp2){
                                         duplicate = true;
                                         break;
@@ -172,9 +199,11 @@ public class BTreeSky extends Iterator{
                                 if(oBuf.get_buf_status()){
                                     Scan scd = heap.openScan();
                                     Tuple heap_dup;
+
                                     RID heap_dup_rid = new RID();
                                     //iterate over heap file.
                                     while((heap_dup = scd.getNext(heap_dup_rid))!= null){
+                                        heap_dup.setHdr(this._len_in1, this._in_1, this._t1_str_sizes);
                                         if(heap_dup == temp2){
                                             duplicate = true;
                                             break;
@@ -184,7 +213,9 @@ public class BTreeSky extends Iterator{
                                 }
                             }
                             if(!duplicate){
-                                oBuf.Put(temp2);
+                                    //remove this later
+
+                                this.oBuf.Put(temp2);
                             }
                         }
                     }
@@ -198,7 +229,8 @@ public class BTreeSky extends Iterator{
 
             //flush the buffer and store to heapfile
             oBuf.flush();
-            
+            free_buffer_pages(this._n_Pages, bufferPIDs);
+
         }
         @Override
         public Tuple get_next() throws Exception{
@@ -219,4 +251,6 @@ public class BTreeSky extends Iterator{
 
 
         }
+
+
 }
