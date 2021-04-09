7533384222c10c3ab19d3dd7e0d9adb59d59744b
Jasmin Trada <Jasmin Trada@DESKTOP-5A0RCDG>
2021-04-09 00:39:07 -0700

    AggType

---
+++ minjava/javaminibase/src/global/AggType.java
@@ -0,0 +1,16 @@
+package global;
+
+public class AggType {
+
+	public static final int aggMax = 0;
+	public static final int aggMin = 1;
+	public static final int aggAvg = 2;
+	public static final int aggSky = 3;
+	
+	public int agg_type;
+	
+	public AggType(int agg_type){
+		this.agg_type = agg_type;
+	}
+	
+}
